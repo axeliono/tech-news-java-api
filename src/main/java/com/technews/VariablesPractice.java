7767685d326e37bd833535bd77355c41304a95ce
axeliono <69375446+axeliono@users.noreply.github.com>
2021-04-29 20:26:39 -0500

    message

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int num1 = 1;
+        double num2 = 1.5;
+        double sum = num1 + num2;
+        System.out.println(sum);
+
+        String word = "word";
+        System.out.println(word + num1);
+    }
+}
